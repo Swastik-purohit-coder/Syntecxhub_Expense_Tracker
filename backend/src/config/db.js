ea712d0417d90b731cff05524688bac993cca15f
swastik <swastikpurohit2007@gmail.com>
2026-04-04 23:48:13 +0530

    fix railway start script

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,31 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "expense_tracker",
-  password: "123456",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
+
+pool.query(`
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  google_id VARCHAR(255),
+  name VARCHAR(255),
+  email VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS expenses (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  title VARCHAR(255),
+  amount NUMERIC,
+  category VARCHAR(100),
+  type VARCHAR(50),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`)
+  .then(() => console.log("Tables created ✅"))
+  .catch((err) => console.log("DB Error ❌", err));
